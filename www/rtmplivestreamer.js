0e131ce3eb3ae5ae9ff89720b5871b8719a0b789
Varsity Software <jweaver@iemic.com>
2019-01-15 17:40:32 -0500

    Update rtmplivestreamer.js

--- www/rtmplivestreamer.js
+++ www/rtmplivestreamer.js
@@ -9,12 +9,47 @@ var RTMPLiveStreamer = function() {
 
 };
 
-RTMPLiveStreamer.prototype.launch = function(success, fail, options) {
+RTMPLiveStreamer.prototype.start = function (success, fail, options)
+{
 	if (!options) {
 		options = {};
-	}
+    }
 
-	return cordova.exec(success, fail, "RTMPLiveStreamer", "launch", [options]);
+    console.log("RTMPLiveStreamer.prototype.start");
+
+    return cordova.exec(success, fail, "RTMPLiveStreamer", "start", [options]);
+};
+
+RTMPLiveStreamer.prototype.stop = function (success, fail, options) {
+    if (!options) {
+        options = {};
+    }
+
+    return cordova.exec(success, fail, "RTMPLiveStreamer", "stop", [options]);
+};
+
+RTMPLiveStreamer.prototype.addQuestionsToList = function (success, fail, options) {
+    if (!options) {
+        options = {};
+    }
+
+    return cordova.exec(success, fail, "RTMPLiveStreamer", "addQuestionsToList", [options]);
+};
+
+RTMPLiveStreamer.prototype.updateViewerCount = function (success, fail, options) {
+    if (!options) {
+        options = {};
+    }
+
+    return cordova.exec(success, fail, "RTMPLiveStreamer", "updateViewerCount", [options]);
+};
+
+RTMPLiveStreamer.prototype.forceQuit = function (success, fail, options) {
+    if (!options) {
+        options = {};
+    }
+
+    return cordova.exec(success, fail, "RTMPLiveStreamer", "forceQuit", [options]);
 };
 
 window.rtmpLiveStreamer = new RTMPLiveStreamer();
